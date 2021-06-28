a047e4243ec56f89ada1d2a0986aafaff8d3f87f
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-06-28 20:04:46 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/felipekonig/course/entities/User.java
+++ src/main/java/com/felipekonig/course/entities/User.java
@@ -1,18 +1,22 @@
 package com.felipekonig.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User implements Serializable{
-
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,7 +24,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -30,6 +37,23 @@ public class User implements Serializable{
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
 	}
 
 	public Long getId() {
@@ -72,21 +96,8 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return id == other.id;
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 }
